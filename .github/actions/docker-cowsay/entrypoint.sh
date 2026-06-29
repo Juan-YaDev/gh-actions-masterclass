b14f74ea6f5782971f291c876d447748e67c2fd2
juancvaldivia <juancvaldivia@bcp.com.pe>
2026-06-29 14:53:32 -0500

    chore: update Dockerfile and entrypoint for Docker Cowsay action

--- .github/actions/docker-cowsay/entrypoint.sh
+++ .github/actions/docker-cowsay/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # .github/actions/docker-cowsay/entrypoint.sh
 
-# $1 es el primer argumento pasado en el 'args' del action.yml
-MESSAGE=$1
+MESSAGE="${1:-Muuu!}"
 
-echo "Ejecutando dentro del contenedor Docker..."
-echo "La versión de Alpine es:"
-cat /etc/alpine-release || echo "No es Alpine"
+echo "🐮 Ejecutando dentro del contenedor Docker (Alpine)..."
+echo "========================================"
 
-# Ejecutamos la herramienta que viene en la imagen
+# Ejecutar cowsay
 cowsay "$MESSAGE"
+
+echo "========================================"
